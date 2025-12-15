a923745a2e176842bda0a6033863b0a59872d515
István <sibalinistvan22@gmail.com>
2025-12-15 16:35:59 +0100

    made Element Buffer Object: draws a rectangle (wireframe mode on)

--- src/main.c
+++ src/main.c
@@ -103,15 +103,26 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	// working with the vertices and indices
+	// -------------------------------------
 	// configuring vertex attributes
-	unsigned int VBO, VAO;
+	unsigned int VBO;	// vertex buffer object
+	unsigned int VAO;	// vertex attribute object
+	unsigned int EBO;	// element buffer object
+
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);	
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
 	
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -120,7 +131,7 @@ int main()
 
 	glBindVertexArray(0);
 
-
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	// render loop
 	while(!glfwWindowShouldClose(window))
@@ -137,7 +148,7 @@ int main()
 		// use the shader program
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		
 		// check and call events and swap the buffer
 		glfwPollEvents();
@@ -146,6 +157,7 @@ int main()
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 	
 	glfwTerminate();	
