d8102865a22df4b7c8503e47f76d8d3aa690fce2
István <sibalinistvan22@gmail.com>
2025-12-20 13:19:36 +0100

    added shader.h

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include	<glad/glad.h>
 #include	<GLFW/glfw3.h>
 
+#include	"shader.h"
+
 #include	<stdio.h>
 #include	<stdbool.h>
 #include	<math.h>
---
+++ src/shader.h
@@ -0,0 +1,8 @@
+#ifndef	SHADER_H
+#define	SHADER_H
+
+#include	<glad/glad.h>
+
+#include	<stdio.h>
+
+#endif
